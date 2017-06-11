6646228be402535003f2d031ea41b18b3c4a7501
Nelz <nelz@orionlabs.io>
2017-06-11 11:32:05 -0700

    Benchmarks

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # go-crypto-source
-Options for using `crypto/rand` to build a `math/rand.Source`
+Options for using `crypto/rand` to back a `math/rand.Rand`
 
 ## Slides
 There is not much actual code here, but part of the intention for this repository is to be an educational tool, so there is a slide presentation available in `./.slides/`.
@@ -9,3 +9,16 @@ To run the slides:
 $ go get -u golang.org/x/tools/present
 $ cd $REPO_ROOT/.slides/ && present .
 ```
+
+## Bechmarks
+The benchmarks that are output from the `benchmark_test.go` are illuminating:
+```
+BenchmarkGlobal-4         	50000000	        37.7 ns/op
+BenchmarkNative-4         	100000000	        22.7 ns/op
+BenchmarkSeed-4           	50000000	        23.9 ns/op
+BenchmarkCryptoRead-4     	 2000000	       735 ns/op
+BenchmarkCryptoUnsafe-4   	 2000000	       793 ns/op
+BenchmarkCryptoSafe-4     	 2000000	       867 ns/op
+```
+
+On a developer workstation, it takes nearly a full millisecond to read 8 bytes from `crypto/rand`, which is about 20x slower than the deterministic `math/rand`. This means that if you are looking to build code that needs random data in a super tight loop, perhaps the techniques used in this repository are not appropriate for your use case.
---
+++ pkg/csrc/benchmark_test.go
@@ -0,0 +1,67 @@
+// Copyright 2017 Orion Labs, Inc
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package csrc
+
+import (
+	crand "crypto/rand"
+	"math/rand"
+	"testing"
+	"time"
+)
+
+var prime1000 = 7919
+
+var result int
+
+func BenchmarkGlobal(b *testing.B) {
+	for n := 0; n < b.N; n++ {
+		result = rand.Intn(prime1000)
+	}
+}
+
+func BenchmarkNative(b *testing.B) {
+	random := rand.New(rand.NewSource(time.Now().UnixNano()))
+	for n := 0; n < b.N; n++ {
+		result = random.Intn(prime1000)
+	}
+}
+
+func BenchmarkSeed(b *testing.B) {
+	random := NewCryptoSeededRandom()
+	for n := 0; n < b.N; n++ {
+		result = random.Intn(prime1000)
+	}
+}
+
+func BenchmarkCryptoRead(b *testing.B) {
+	buffer := make([]byte, 8)
+	for n := 0; n < b.N; n++ {
+		result, _ = crand.Read(buffer)
+	}
+}
+
+func BenchmarkCryptoUnsafe(b *testing.B) {
+	random := NewRandom(false)
+	for n := 0; n < b.N; n++ {
+		result = random.Intn(prime1000)
+	}
+}
+
+func BenchmarkCryptoSafe(b *testing.B) {
+	random := NewRandom(true)
+	for n := 0; n < b.N; n++ {
+		result = random.Intn(prime1000)
+	}
+}
